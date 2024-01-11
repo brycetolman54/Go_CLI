8291fc69c9040dd1ab21f9db20d57e4c347abead
brycealantolman@gmail.com <brycealantolman@gmail.com>
2024-01-10 23:40:02 -0700

    update(clear) to remove only done items

--- cmd/clear.go
+++ cmd/clear.go
@@ -4,10 +4,14 @@ Copyright © 2024 NAME HERE <EMAIL ADDRESS>
 package cmd
 
 import (
+	"log"
 	"todo/todoList"
 
 	"github.com/spf13/cobra"
 )
+
+// variable for flags
+var doneOnly bool
 
 // clearCmd represents the clear command
 var clearCmd = &cobra.Command{
@@ -18,11 +22,32 @@ var clearCmd = &cobra.Command{
 }
 
 func runClear(cmd *cobra.Command, args []string) {
-	todoList.SaveItems(dataFile, nil)
+	if doneOnly {
+		// get the items
+		items, err := todoList.ReadItems(dataFile)
+		if err != nil {
+			log.Printf("%v", err)
+		}
+
+		// make a new list that only has the not done items
+		var newItems []todoList.Item
+		for _, i := range items {
+			if !i.Done {
+				newItems = append(newItems, i)
+			}
+		}
+
+		// save this new list
+		todoList.SaveItems(dataFile, newItems)
+	} else {
+		todoList.SaveItems(dataFile, nil)
+	}
 }
 
 func init() {
 	rootCmd.AddCommand(clearCmd)
+
+	clearCmd.Flags().BoolVarP(&doneOnly, "done", "d", false, "Clear any items that are marked done from the Todo list")
 
 	// Here you will define your flags and configuration settings.
 
