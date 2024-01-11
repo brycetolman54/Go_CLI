4f9e68d858e23e77629b45c33775abb96f97dfb4
brycealantolman@gmail.com <brycealantolman@gmail.com>
2024-01-10 23:48:49 -0700

    remove unnecessary comments

--- cmd/clear.go
+++ cmd/clear.go
@@ -49,13 +49,4 @@ func init() {
 
 	clearCmd.Flags().BoolVarP(&doneOnly, "done", "d", false, "Clear any items that are marked done from the Todo list")
 
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// clearCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// clearCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
--- cmd/root.go
+++ cmd/root.go
@@ -14,6 +14,7 @@ import (
 // file names for later
 var dataFile string
 var errorFile string
+var cfgFile string
 
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
@@ -91,8 +92,4 @@ func init() {
 	// -----------------------------------------------------------------
 
 	// rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.todo.yaml)")
-
-	// Cobra also supports local flags, which will only run
-	// when this action is called directly.
-	// rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
